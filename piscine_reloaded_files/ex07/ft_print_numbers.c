d34e91caacf4e2c3beadc84b6e613327482c97a0
Pieter Claus <pieter.claus1@gmail.com>
2023-10-10 22:29:29 +0200

    add ++ in the same line

--- piscine_reloaded_files/ex07/ft_print_numbers.c
+++ piscine_reloaded_files/ex07/ft_print_numbers.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 22:22:32 by pclaus            #+#    #+#             */
-/*   Updated: 2023/10/10 22:25:51 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/10/10 22:28:52 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,10 @@ void	ft_print_numbers(void)
 
 	number = '0';
 	while (number <= '9')
-	{
-		ft_putchar(number);
-		number++;
-	}
+		ft_putchar(number++);
 }
-/*
+
 int	main(void)
 {
 	ft_print_numbers();
-}*/
+}
