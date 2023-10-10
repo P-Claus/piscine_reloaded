9c5470e66cbf72b819c5261ccba0e2e2d7637da0
Pieter Claus <pieter.claus1@gmail.com>
2023-10-10 22:26:24 +0200

    finish ex07

---
+++ piscine_reloaded_files/ex07/ft_print_numbers.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/10 22:22:32 by pclaus            #+#    #+#             */
+/*   Updated: 2023/10/10 22:25:51 by pclaus           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print_numbers(void)
+{
+	int	number;
+
+	number = '0';
+	while (number <= '9')
+	{
+		ft_putchar(number);
+		number++;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_numbers();
+}*/
