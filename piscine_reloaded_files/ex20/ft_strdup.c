e6824292a60f5b7a0b780bf74a665f4fd63e7d01
Pieter Claus <pieter.claus1@gmail.com>
2023-10-13 17:52:58 +0200

    finish ex20

--- piscine_reloaded_files/ex20/ft_strdup.c
+++ piscine_reloaded_files/ex20/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 21:54:35 by pclaus            #+#    #+#             */
-/*   Updated: 2023/10/13 17:14:51 by pclaus           ###   ########.fr       */
+/*   Updated: 2023/10/13 17:51:58 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,11 @@ char	*ft_strdup(char *src)
 	int		counter;
 	char	*dst;
 
-	len = ft_strlen(src);
+	len = ft_strlen(src) + 1;
 	counter = 0;
 	dst = (char *) malloc(len * sizeof(char));
+	if (dst == 0)
+		return (0);
 	while (*(src + counter) != '\0')
 	{
 		*(dst + counter) = *(src + counter);
@@ -39,7 +41,7 @@ char	*ft_strdup(char *src)
 	*(dst + counter) = '\0';
 	return (dst);
 }
-/*
+
 #include <stdio.h>
 #include <string.h>
 int	main(void)
@@ -48,4 +50,4 @@ int	main(void)
 
 	printf("Using strdup:  %s\n", strdup(src));
 	printf("Using ft_strdup:  %s", ft_strdup(src));
-}*/
+}
