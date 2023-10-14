9f51dd60ea4bac8b8ea82fe657415a4b1586b46e
Pieter Claus <pieter.claus1@gmail.com>
2023-10-14 14:44:28 +0200

    work on ex24 makefile

---
+++ piscine_reloaded_files/ex24/includes/libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pclaus <pclaus@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/14 13:07:01 by pclaus            #+#    #+#             */
+/*   Updated: 2023/10/14 13:15:00 by pclaus           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char *s);
+void	ft_swap(int *a, int *b);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_strlen(char *str);
+
+#endif
